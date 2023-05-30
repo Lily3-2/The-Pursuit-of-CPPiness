dc3dc8af084ef9b1654358ee69be092507c95659
Lily3-2 <sbbm.03@gmail.com>
2023-05-30 16:24:07 +0545

    insertion sort

---
+++ 18_insertion_sort.cpp
@@ -0,0 +1,36 @@
+/*Insertion Sort
+TC = O(n^2) for average and worst case
+TC = O(n) for best case
+SC = O(1)
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int arr[], int n) {
+  for(int i=0; i<n; i++) {
+    int j=i;
+    while(j>0 && arr[j-1]>arr[j]) {
+      int temp = arr[j-1];
+      arr[j-1]= arr[j];
+      arr[j]= temp;
+      j--;
+    }
+  }
+}
+
+int main() {
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0; i<n; i++) {
+    cin>>arr[i];
+  }
+  insertion_sort(arr, n);
+  for(int i=0; i<n; i++) {
+    cout<<arr[i]<<" ";
+  }
+  return 0;
+}
