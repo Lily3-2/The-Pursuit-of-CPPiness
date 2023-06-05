c1be08d4ff5c134faae861fef315c1571f7d811b
Lily3-2 <sbbm.03@gmail.com>
2023-06-05 22:41:02 +0545

    remove duplicates

---
+++ 25_remove_duplicates_array.cpp
@@ -0,0 +1,27 @@
+//from an array display the first k unique elements and with redundant remaining element and return k
+//shift or remove the normal order into the order of the unique elements
+//using hasmap to store elements and its frequency
+#include <bits/stdc++.h>
+using namespace std;
+//not an in-place approach
+int unique_ele(vector<int> &arr) {
+  int k;
+  map<int, int> mpp;
+  for(int i=0; i<arr.size(); i++) {
+    mpp[arr[i]]++;
+  }
+  int count = 0;
+
+  for(auto itr: mpp) {
+      count++;
+  }
+  return count;
+
+}
+int main() {
+  vector<int> arr {1, 1, 2, 2, 2, 3 ,3};
+  cout << unique_ele(arr);
+  return 0;
+
+}
+
