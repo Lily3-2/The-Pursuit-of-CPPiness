b0f02c051ce20211b70c1b44cbd1d5f58f3a712e
Lily3-2 <sbbm.03@gmail.com>
2023-07-10 23:41:43 +0545

    reverse words in a sentence

--- 151_reverse_words.cpp
+++ 151_reverse_words.cpp
@@ -1,35 +1,41 @@
 // LC - 151. Reverse Words in a String
 
 
-class Solution {
-public:
-    string reverseWords(string s) {
-       string temp = "";
-       stack<string>st;
-       for(int i=0; i<s.size();i++){
-           char ch = s[i];
-           if(ch == ' '){
-               if(temp != "")
-                   st.push(temp);
-               temp = "";
-           }
+#include <bits/stdc++.h>
+using namespace std;
 
-           else
-            temp+=ch;
-       }
+string reverseWords(string s) {
+    string temp = "";
+    stack<string>st;
+    for(int i=0; i<s.size();i++){
+        char ch = s[i];
+        if(ch == ' '){
+            if(temp != "")
+                st.push(temp);
+            temp = "";
+        }
 
-       if(temp!=""){
-           st.push(temp);
-       }
-       string ans = "";
-
-       while(!st.empty()){
-           ans+=(st.top()+" ");
-           st.pop();
-       }
-       ans.pop_back();
-
-       return ans;
-      
+        else
+        temp+=ch;
     }
+
+    if(temp!=""){
+        st.push(temp);
+    }
+    string ans = "";
+
+    while(!st.empty()){
+        ans+=(st.top()+" ");
+        st.pop();
+    }
+    ans.pop_back();
+
+    return ans;
+    
+}
+int main() {
+    string s;
+    getline(cin, s); // read the entire line of input even the whitespaces
+    cout << reverseWords(s);
+    return 0;
 }
