ad555904c19156867f657757c0aa6e8894e295fe
Lily3-2 <sbbm.03@gmail.com>
2023-08-21 23:35:44 +0545

    check num is power of 2

--- 231_powerOf2.cpp
+++ 231_powerOf2.cpp
@@ -2,24 +2,38 @@
 
 // An integer n is a power of two, if there exists an integer x such that n == 2x.
 
-// TC = O() , SC = O()
+// TC = O(1) , SC = O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
 
+//loagrithmic approach 
 bool isPowerOfTwo(int n) {
-  int temp = 1;
-    while( temp <= n){
-        if(temp == n) {
-            return true;
-        }
-        temp = temp * 2;
-    }
+  if(n<= 0) {
+    return false;
+  }
+  double log2num = log2(n);
+  if(log2num == floor(log2num)) {
+    return true;
+  }
   return false;
 }
 
+
+// --- TC = O(logn) SC = O(1)  but this approach will have error if the n value will exceed the MAX_INT value
+// bool isPowerOfTwo(int n) {
+//   int temp = 1;
+//     while( temp <= n){
+//         if(temp == n) {
+//             return true;
+//         }
+//         temp = temp * 2;
+//     }
+//   return false;
+// }
+
 int main() {
-  int n = 5;
+  int n = 64;
   if(isPowerOfTwo(n)) {
     cout<< "true";
   }
